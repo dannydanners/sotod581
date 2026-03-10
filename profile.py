ad102e54fa95582dc0161a1cffdda938f859d248
Daniel Soto <dan.soto14@gmail.com>
2026-03-10 01:14:40 -0400

    docker install

--- profile.py
+++ profile.py
@@ -8,12 +8,38 @@ node = request.XenVM("node")
 node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU22-64-STD"
 node.routable_control_ip = "true"
 
-node.addService(rspec.Execute(shell="/bin/sh", command="sudo apt update"))
-node.addService(rspec.Execute(shell="/bin/sh", command="sudo apt install -y apache2"))
-node.addService(rspec.Execute(shell="/bin/sh", command='sudo systemctl status apache2'))
-node.addService(rspec.Execute(shell="/bin/sh", command="sudo apt install -y curl"))
-node.addService(rspec.Execute(shell="/bin/sh", command='curl -fsSL https://get.docker.com -o get-docker.sh'))
-node.addService(rspec.Execute(shell="/bin/sh", command='sudo sh get-docker.sh'))
+node.addService(rspec.Execute(
+    shell="/bin/sh",
+    command="""
+    sudo apt update
+    sudo apt install ca-certificates curl gnupg
+    sudo install -m 0755 -d /etc/apt/keyrings
+    sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+    sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+    sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+    Types: deb
+    URIs: https://download.docker.com/linux/ubuntu
+    Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
+    Components: stable
+    Signed-By: /etc/apt/keyrings/docker.asc
+    EOF
+
+    sudo apt update
+    sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    
+    sudo systemctl enable docker
+    sudo systemctl start docker
+
+    sudo docker version
+    sudo docker compose version
+    """
+    ))
+#node.addService(rspec.Execute(shell="/bin/sh", command="sudo apt install -y apache2"))
+#node.addService(rspec.Execute(shell="/bin/sh", command='sudo systemctl status apache2'))
+#node.addService(rspec.Execute(shell="/bin/sh", command="sudo apt install -y curl"))
+#node.addService(rspec.Execute(shell="/bin/sh", command='curl -fsSL https://get.docker.com -o get-docker.sh'))
+#node.addService(rspec.Execute(shell="/bin/sh", command='sudo sh get-docker.sh'))
 
 # Print the RSpec to the enclosing page.
 portal.context.printRequestRSpec()
