bdd6f0f39cefb7a79d1ecd4f3778682b946249fc
Daniel Soto <dan.soto14@gmail.com>
2026-03-16 00:42:26 -0400

    update script

--- profile.py
+++ profile.py
@@ -11,31 +11,32 @@ node.routable_control_ip = "true"
 node.addService(rspec.Execute(
     shell="/bin/sh",
     command="""
-    sudo apt update
-    sudo apt install ca-certificates curl gnupg
-    sudo install -m 0755 -d /etc/apt/keyrings
-    sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-    sudo chmod a+r /etc/apt/keyrings/docker.asc
+set -eux
+sudo apt update
+sudo apt-get install -y ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-    sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
-    Types: deb
-    URIs: https://download.docker.com/linux/ubuntu
-    Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
-    Components: stable
-    Signed-By: /etc/apt/keyrings/docker.asc
-    EOF
+sudo sh -c 'printf "%s\n" \
+"Types: deb" \
+"URIs: https://download.docker.com/linux/ubuntu" \
+"Suites: jammy" \
+"Components: stable" \
+"Signed-By: /etc/apt/keyrings/docker.asc" \
+> /etc/apt/sources.list.d/docker.sources'
 
-    sudo apt update
-    sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-    
-    sudo systemctl enable docker
-    sudo systemctl start docker
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-    sudo docker version
-    sudo docker compose version
+sudo systemctl enable docker
+sudo systemctl start docker
 
-    cd /local/repository
-    sudo docker compose up -d
+sudo docker version
+sudo docker compose version
+
+cd /local/repository
+sudo docker compose up -d
     """
     ))
 #node.addService(rspec.Execute(shell="/bin/sh", command="sudo apt install -y apache2"))
