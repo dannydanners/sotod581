0d71b6ce1e0759903553d5f9af1478dc9b705cab
Daniel Soto <dan.soto14@gmail.com>
2026-03-18 05:05:05 -0400

    docker install

--- profile.py
+++ profile.py
@@ -1,49 +1,32 @@
 import geni.portal as portal
-import geni.rspec.pg as rspec
+import geni.rspec.pg as pg
+import geni.rspec.igext as IG
 
-# Create a Request object to start building the RSpec.
-request = portal.context.makeRequestRSpec()
-# Create a XenVM
-node = request.XenVM("node")
-node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU22-64-STD"
+pc = portal.Context()
+request = pc.makeRequestRSpec()
+
+tourDescription = \
+"""
+This profile provides the template for a compute node with Docker installed on Ubuntu 18.04
+"""
+
+#
+# Setup the Tour info with the above description and instructions.
+#  
+tour = IG.Tour()
+tour.Description(IG.Tour.TEXT,tourDescription)
+request.addTour(tour)
+
+node = request.XenVM("docker")
+node.cores = 8
+node.ram = 8192
+node.routable_control_ip = "true" 
+
+bs_landing = node.Blockstore("bs_image", "/image")
+bs_landing.size = "500GB"
+  
+node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
 node.routable_control_ip = "true"
-
-node.addService(rspec.Execute(
-    shell="/bin/bash",
-    command="""
-set -eux;
-sudo apt update;
-sudo apt-get install -y ca-certificates curl gnupg;
-sudo install -m 0755 -d /etc/apt/keyrings;
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc;
-sudo chmod a+r /etc/apt/keyrings/docker.asc;
-
-sudo sh -c 'printf "%s\n" \
-"Types: deb" \
-"URIs: https://download.docker.com/linux/ubuntu" \
-"Suites: jammy" \
-"Components: stable" \
-"Signed-By: /etc/apt/keyrings/docker.asc" \
-> /etc/apt/sources.list.d/docker.sources';
-
-sudo apt-get update;
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin;
-
-sudo systemctl enable docker;
-sudo systemctl start docker;
-
-sudo docker version;
-sudo docker compose version'
-
-cd /local/repository;
-sudo docker compose up -d
-    """
-    ))
-#node.addService(rspec.Execute(shell="/bin/sh", command="sudo apt install -y apache2"))
-#node.addService(rspec.Execute(shell="/bin/sh", command='sudo systemctl status apache2'))
-#node.addService(rspec.Execute(shell="/bin/sh", command="sudo apt install -y curl"))
-#node.addService(rspec.Execute(shell="/bin/sh", command='curl -fsSL https://get.docker.com -o get-docker.sh'))
-#node.addService(rspec.Execute(shell="/bin/sh", command='sudo sh get-docker.sh'))
-
-# Print the RSpec to the enclosing page.
-portal.context.printRequestRSpec()
+node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/install_docker.sh"))
+  
+pc.printRequestRSpec(request)
